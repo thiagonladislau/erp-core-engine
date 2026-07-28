c38dc548af763660029015a8515ea1948e131de0
Thiago Ladislau <thiagoladislau@yahoo.com.br>
2026-07-28 11:30:14 -0300

    Feat: initial commit whith health check endpoint

---
+++ src/main/java/com/ladislau/erp/coreengine/CoreEngineApplication.java
@@ -0,0 +1,13 @@
+package com.ladislau.erp.coreengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ladislau/erp/coreengine/CoreEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ladislau.erp.coreengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
